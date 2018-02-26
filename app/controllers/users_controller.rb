ae687f0c464cf6c0b2dbe3992fab1952f953e84b
coadyduffney <coadyduffney@gmail.com>
2018-02-26 19:26:36 +0000

    Fixing Friends Display Bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
   end
   
   def my_friends
-    
+    @friendships = current_user.friends
   end
   
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = "No users found" if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
 end
